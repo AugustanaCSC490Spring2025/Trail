6db2471b90fdcc2bbdd0152e699c1afc42a29b81
maxsellers <63917117+maxsellers@users.noreply.github.com>
2025-03-18 12:11:07 -0500

    Proc Gen part 2 added terrain and buildings

--- Scripts/map.gd
+++ Scripts/map.gd
@@ -2,16 +2,19 @@ extends Node2D
 
 
 
+
 @export var noise_texture : NoiseTexture2D
 @export var tree_noise_texture : NoiseTexture2D
 
-var width : int = 100
-var height : int =  100
+var width : int = 200
+var height : int =  200
 
 var noise : Noise
 var tree_noise : Noise
 
 var water_tile_atlas = Vector2i(0,1)
+var house_atlas1 = [Vector2i(1,1), Vector2i(9,1), Vector2i(20,2), Vector2i(27,1), Vector2i(34,1), Vector2i(41,1), Vector2i(1,12), Vector2i(15,11), Vector2i(22,10)]
+var house_atlas2 = Vector2i(9,1)
 var tree_atlas = Vector2i(12,2)
 var tree_atlas2 = Vector2i(15,6)
 
@@ -19,8 +22,8 @@ var tree_atlas2 = Vector2i(15,6)
 var sand_arr = []
 var grass_arr = []
 var dirt_arr = []
-var cliff_arr = []
-
+var building_arr = []
+var tree_noise_val_arr = []
 ################################
 #@onready var tile_map = $TileMap
 #
@@ -31,19 +34,23 @@ var cliff_arr = []
 #var cliff_layer = 3
 #var environment_layer =4
 ################################
-@onready var ground_tilemaplayer: TileMapLayer = $Ground
-
-
+@onready var grass_tilemaplayer: TileMapLayer = $Grass
+@onready var dirt_tilemaplayer: TileMapLayer = $Dirt
+@onready var object_tilemaplayer: TileMapLayer = $Object
 
 var random_grass_atlas_arr = [Vector2i(1,0),Vector2i(2,0),Vector2i(3,0),Vector2i(4,0),Vector2i(5,0)]
 @onready var camera_2d = $Player/Camera2D
 
-
 func _ready():
+	var rng: int = randi_range(0,100)
 	noise = noise_texture.noise
+	noise.set_seed(rng)
+	print("seed ", rng)
 	tree_noise = tree_noise_texture.noise
 	generate_world()
 	
+# lowest noise: -.6271
+# highest noise: .4845
 func generate_world():
 	var noise_val
 	var tree_noise_val 
@@ -51,10 +58,29 @@ func generate_world():
 		for y in range(-height/2, height/2):
 			noise_val = noise.get_noise_2d(x,y)
 			tree_noise_val = tree_noise.get_noise_2d(x,y)
+			if noise_val > -0.2:
+				dirt_arr.append(Vector2(x,y))
+				if len(building_arr) < 20: 
+					var building_eligible: bool = true
+					if len(building_arr) == 0:
+						if ((x<-90 or x>90) or (y<-90 or y>90)):
+							building_eligible = false
+					else:
+						for item in building_arr:
+							if (item.distance_to(Vector2(x,y)) < 30 or ((x<-90 or x>90) or (y<-90 or y>90))):
+								building_eligible = false
+					if building_eligible == true and randi_range(1,10) == 1:
+							print("distance", Vector2(x,y))
+							building_arr.append(Vector2(x,y))
+							object_tilemaplayer.set_cell(Vector2(x,y), 0,house_atlas1.pick_random())
 			grass_arr.append(Vector2(x,y))
-			ground_tilemaplayer.set_cell(Vector2(x,y), 0,random_grass_atlas_arr.pick_random())
+			tree_noise_val_arr.append(tree_noise_val)
+			
+	print("highest", tree_noise_val_arr.max())
+	print("lowest", tree_noise_val_arr.min())
 	
-	ground_tilemaplayer.set_cells_terrain_connect(grass_arr, 0,0)
+	grass_tilemaplayer.set_cells_terrain_connect(grass_arr, 0,0)
+	dirt_tilemaplayer.set_cells_terrain_connect(dirt_arr, 1,0)
 
 func _input(event):
 	if Input.is_action_just_pressed("zoom_in"):
@@ -66,3 +92,4 @@ func _input(event):
 			zoom_val =camera_2d.zoom.x - 0.2
 			
 		camera_2d.zoom = Vector2(zoom_val, zoom_val)
+		print(camera_2d.get_screen_center_position())
