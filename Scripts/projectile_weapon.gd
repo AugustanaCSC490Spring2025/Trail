de14b8d95419b7c501b03c303ea2056c38394b6c
Bill Unyi <billunyi@gmail.com>
2025-03-24 12:27:28 -0500

    Weapon now always faces up right

--- Scripts/projectile_weapon.gd
+++ Scripts/projectile_weapon.gd
@@ -1,12 +1,23 @@
 extends Node2D
 
 var mouse_position =  null
+var side = true
 @onready var weapon_tip = $RayCast2D/Tip
 @onready var pointing_tip = $RayCast2D
 @onready var pivot_point = $"."
+@onready var sprite = $Sprite2D
 
 func _physics_process(delta: float) -> void:
 	mouse_position = get_global_mouse_position()
 	pivot_point.look_at(mouse_position)
+	if((pivot_point.global_position.x > mouse_position.x && side) || (pivot_point.global_position.x < mouse_position.x && !side)):
+		side = !side
+		flip()
+		
 	#weapon_tip.position = mouse_position
 	#pointing_tip.target_position = mouse_position
+
+func flip():
+	sprite.scale.y *= -1
+	sprite.position.y *= -1
+	print("Filp")
